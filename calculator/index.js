5e4ba411a531e013f6fb43ea1e2c3573fee6c2a8
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2023-04-01 14:02:46 +0700

    base application

---
+++ calculator/index.js
@@ -0,0 +1,60 @@
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	if (b === 0) {
+		throw new Error('Cannot divide by zero');
+	}
+	return a / b;
+}
+
+let result = 0;
+let operation = '';
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let output = document.getElementById('output');
+let addButton = document.getElementById('addButton');
+let subtractButton = document.getElementById('subtractButton');
+let multiplyButton = document.getElementById('multiplyButton');
+let divideButton = document.getElementById('divideButton');
+
+addButton.addEventListener('click', () => {
+	let a = parseFloat(input1.value);
+	let b = parseFloat(input2.value);
+	result = add(a, b);
+	operation = '+';
+	output.value = result;
+});
+
+subtractButton.addEventListener('click', () => {
+	let a = parseFloat(input1.value);
+	let b = parseFloat(input2.value);
+	result = subtract(a, b);
+	operation = '-';
+	output.value = result;
+});
+
+multiplyButton.addEventListener('click', () => {
+	let a = parseFloat(input1.value);
+	let b = parseFloat(input2.value);
+	result = multiply(a, b);
+	operation = '*';
+	output.value = result;
+});
+
+divideButton.addEventListener('click', () => {
+	let a = parseFloat(input1.value);
+	let b = parseFloat(input2.value);
+	result = divide(a, b);
+	operation = '/';
+	output.value = result;
+});
